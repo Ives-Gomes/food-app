b47fa3e00aca28fa1d07883087773db7cfc9e9bf
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-03-10 14:02:32 -0300

    feat: add multiples items in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Modal from '../Modal/Modal';
+import CartItem from './CartItem/CartItem';
+
+import CartContext from '../../store/Cart/CartContext';
 
 import classes from './style.module.css';
 
 function Cart({ onClose }) {
-  const cartItems = [
-    {
-      id: 'c1',
-      name: 'Sushi',
-      amount: 2,
-      price: 12.99,
-    },
-  ];
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
 
   return (
     <Modal onClose={onClose}>
-      {cartItems.map((item) => (
-        <ul className={classes['cart-items']}>
-          <li>{item.name}</li>
-        </ul>
-      ))}
+      <ul className={classes['cart-items']}>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            // eslint-disable-next-line react/jsx-no-bind
+            onAdd={cartItemAddHandler.bind(null, item.id)}
+            // eslint-disable-next-line react/jsx-no-bind
+            onRemove={cartItemRemoveHandler.bind(null, item)}
+          />
+        ))}
+      </ul>
 
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
 
       <div className={classes.actions}>
         <button type="button" className={classes['button--alt']} onClick={onClose}>
           Close
         </button>
-        <button type="button" className={classes.button}>Order</button>
+
+        {hasItems && <button type="button" className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/store/Cart/CartProvider.jsx
+++ src/store/Cart/CartProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useMemo } from 'react';
 
 import CartContext from './CartContext';
 
@@ -9,8 +9,25 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,
@@ -32,12 +49,12 @@ function CartProvider({ children }) {
     dispatchCartAction({ type: 'REMOVE', id });
   };
 
-  const cartContext = {
+  const cartContext = useMemo({
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
-  };
+  });
 
   return (
     <CartContext.Provider value={cartContext}>
